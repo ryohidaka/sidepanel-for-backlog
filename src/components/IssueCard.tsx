08e0464c2e7193526fb771928bb2217832dbdb82
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-03-24 00:09:14 +0900

    feat: 課題カードコンポーネントを追加

---
+++ src/components/IssueCard.tsx
@@ -0,0 +1,29 @@
+import { Card, CardBody, Heading } from "@chakra-ui/react"
+import type { Issue } from "backlog-js/dist/types/entity"
+
+type Props = {
+  issue: Issue.Issue // バックログの課題情報
+}
+
+/**
+ * IssueCardコンポーネント
+ * バックログの課題情報を表示するカードコンポーネント
+ * @param {Props} props - バックログの課題情報
+ * @returns {JSX.Element}
+ */
+function IssueCard({ issue }: Props): JSX.Element {
+  return (
+    <Card as="article">
+      <CardBody p={2} minH={12}>
+        <Heading as="h2" size="xs" noOfLines={2}>
+          {/* 課題キー */}
+          {issue.issueKey}
+          {/* 課題タイトル */}
+          {issue.summary}
+        </Heading>
+      </CardBody>
+    </Card>
+  )
+}
+
+export default IssueCard
