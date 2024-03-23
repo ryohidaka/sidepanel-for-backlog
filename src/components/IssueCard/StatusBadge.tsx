27e3e95df3c41117cbbccff41d56f24407641c56
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-03-24 01:21:26 +0900

    feat: 課題の状態を示すバッジを追加

---
+++ src/components/IssueCard/StatusBadge.tsx
@@ -0,0 +1,22 @@
+import { Badge } from "@chakra-ui/react"
+import type { Issue } from "backlog-js/dist/types/entity"
+
+type Props = {
+  status: Issue.Issue["status"]
+}
+
+/**
+ * StatusBadgeコンポーネント
+ * @param {Props} props - Propsのオブジェクト
+ * @param {Issue.Issue["status"]} props.status - ステータスのオブジェクト
+ * @returns {JSX.Element}
+ */
+function StatusBadge({ status }: Props): JSX.Element {
+  return (
+    <Badge bgColor={status.color} color="white">
+      {status.name}
+    </Badge>
+  )
+}
+
+export default StatusBadge
--- src/components/IssueCard/index.tsx
+++ src/components/IssueCard/index.tsx
@@ -1,7 +1,8 @@
-import { Card, CardBody, Heading } from "@chakra-ui/react"
+import { Card, CardBody, Heading, HStack, Stack } from "@chakra-ui/react"
 import type { Issue } from "backlog-js/dist/types/entity"
 
 import IssueKey from "./IssueKey"
+import StatusBadge from "./StatusBadge"
 
 type Props = {
   issue: Issue.Issue // バックログの課題情報
@@ -17,12 +18,19 @@ function IssueCard({ issue }: Props): JSX.Element {
   return (
     <Card as="article">
       <CardBody p={2} minH={12}>
-        <Heading as="h2" size="xs" noOfLines={2}>
-          {/* 課題キー */}
-          <IssueKey issueKey={issue.issueKey} />
-          {/* 課題タイトル */}
-          {issue.summary}
-        </Heading>
+        <Stack spacing={2}>
+          <Heading as="h1" size="xs" noOfLines={2}>
+            {/* 課題キー */}
+            <IssueKey issueKey={issue.issueKey} />
+            {/* 課題タイトル */}
+            {issue.summary}
+          </Heading>
+
+          <HStack justifyContent="end">
+            {/* 状態 */}
+            <StatusBadge status={issue.status} />
+          </HStack>
+        </Stack>
       </CardBody>
     </Card>
   )
