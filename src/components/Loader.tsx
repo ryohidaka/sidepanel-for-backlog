17b50d4c3ab711fc43f7c52457b79bdf7e266341
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-03-24 00:24:31 +0900

    feat: ローディングコンポーネントを追加

---
+++ src/components/Loader.tsx
@@ -0,0 +1,18 @@
+import { AbsoluteCenter, Spinner, Stack } from "@chakra-ui/react"
+
+/**
+ * Loaderコンポーネント
+ * ローディング中に表示するスピナーを中央に配置するコンポーネント
+ * @returns {JSX.Element} レンダリング結果
+ */
+function Loader(): JSX.Element {
+  return (
+    <Stack position="fixed" w="100%" h="100%" top={0}>
+      <AbsoluteCenter>
+        <Spinner size="xl" />
+      </AbsoluteCenter>
+    </Stack>
+  )
+}
+
+export default Loader
