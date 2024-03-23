f44ae2c4f97a2710a6efc52abf45354cc2fd4ac5
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-03-23 10:44:25 +0900

    feat: 入力フォームコンポーネントを追加

---
+++ src/components/InputForm.tsx
@@ -0,0 +1,43 @@
+import { FormControl, FormLabel, Input } from "@chakra-ui/react"
+import type { HTMLInputTypeAttribute } from "react"
+
+type Props = {
+  name: string // 入力フィールドの名前
+  label: string // フィールドのラベル
+  defaultValue: string // デフォルトの値
+  type?: HTMLInputTypeAttribute // 入力フィールドのタイプ
+  onChange?: (newValue: string) => void // 値が変更されたときのハンドラ
+}
+
+/**
+ * 入力フォームコンポーネント
+ * @param {object} props - コンポーネントのプロパティ
+ * @param {string} props.name - 入力フィールドの名前
+ * @param {string} props.label - フィールドのラベル
+ * @param {string} props.defaultValue - デフォルトの値
+ * @param {HTMLInputTypeAttribute} props.type - 入力フィールドのタイプ
+ * @param {(newValue: string) => void} props.onChange - 値が変更されたときのハンドラ
+ * @returns {JSX.Element} 入力フォームのエレメント
+ */
+function InputForm({
+  name,
+  label,
+  defaultValue = "",
+  type = "text",
+  onChange
+}: Props): JSX.Element {
+  return (
+    <FormControl>
+      <FormLabel htmlFor={name}>{label}</FormLabel>
+      <Input
+        id={name} // htmlForと一致させるためにidを追加
+        name={name}
+        type={type}
+        defaultValue={defaultValue} // 制御されていないコンポーネントにするためにvalueからdefaultValueに変更
+        onChange={(e) => onChange && onChange(e.target.value)} // onChangeが存在する場合のみ呼び出す
+      />
+    </FormControl>
+  )
+}
+
+export default InputForm
